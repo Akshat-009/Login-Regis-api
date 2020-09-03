80a6facdc21e4787028f5e1f33bd4e27a6ca5362
akshat <akshat_g@ch.iitr.ac.in>
2020-09-03 22:13:07 +0530

    Connected to database

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
-import dotenv from "dotenv"
-dotenv.config
+import dotenv from 'dotenv'
+dotenv.config()
 
 
 
 const app=express()
 const PORT=process.env.PORT || 9000
 
+
+mongoose.connect(process.env.MONGOOSE_CONNECT_URL,{useUnifiedTopology:true,useNewUrlParser:true,useCreateIndex:true},(err)=>{
+    if (err)
+    {
+        throw err
+    }
+    else{console.log("successfully connected")}
+})
 //MIDDLEWARE    
 app.use(express.json())
 app.use(cors())
